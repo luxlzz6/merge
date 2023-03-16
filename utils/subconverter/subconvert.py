2b383eb747b2ed7ee339fa62297797660a964e40
luxl-1379 <53847251+luxl-1379@users.noreply.github.com>
2023-03-16 12:26:04 +0800

    Update subconvert.py

--- utils/subconverter/subconvert.py
+++ utils/subconverter/subconvert.py
@@ -7,7 +7,7 @@ import socket
 import geoip2.database
 
 
-def convert(subscription,target,other_config={}):
+def convert(subscription, target, other_config={}):
     """Wrapper for subconverter
     subscription: subscription url or content string or local file path, add url support.
     target: target subconvert configuration
@@ -21,15 +21,15 @@ def convert(subscription,target,other_config={}):
 
     default_config = {
         'target': target,
-        'deduplicate':False,'keep_nodes':1,
-        'rename':'','include':'','exclude':'','config':''
+        'deduplicate': False, 'keep_nodes': 1,
+        'rename': '', 'include': '', 'exclude': '', 'config': ''
     }
     default_config.update(other_config)
     config = default_config
-    
+
     work_dir = os.getcwd()
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    
+
     if subscription[:8] == 'https://':
         clash_provider = subconverterhandler(subscription)
     else:
@@ -40,7 +40,7 @@ def convert(subscription,target,other_config={}):
                     subscription = content
                     raise ValueError
                 else:
-                    clash_provider = subconverterhandler(subscription)                   
+                    clash_provider = subconverterhandler(subscription)
         except Exception:
             try:
                 if 'proxies:' not in subscription:
@@ -61,16 +61,17 @@ def convert(subscription,target,other_config={}):
                 return ''
 
     if config['deduplicate']:
-        
-        clash_provider = deduplicate(clash_provider,config['keep_nodes'])
-    with open('./temp', 'w', encoding= 'utf-8') as temp_file:       
+        clash_provider = deduplicate(clash_provider, config['keep_nodes'])
+    with open('./temp', 'w', encoding='utf-8') as temp_file:
         temp_file.write(clash_provider)
-    output = subconverterhandler('./temp',config)
-    
+    output = subconverterhandler('./temp', config)
+
     os.chdir(work_dir)
     return output
 
-def subconverterhandler(subscription,input_config={'target':'transfer','rename':'','include':'','exclude':'','config':''}):
+
+def subconverterhandler(subscription,
+                        input_config={'target': 'transfer', 'rename': '', 'include': '', 'exclude': '', 'config': ''}):
     """Wrapper for subconverter(by configuration file: generate.ini)
     Target handling config parameters(parameters from https://github.com/tindy2013/subconverter/blob/master/README-cn.md#%E8%BF%9B%E9%98%B6%E9%93%BE%E6%8E%A5):
         target: target subconvert configuration
@@ -85,32 +86,35 @@ def subconverterhandler(subscription,input_config={'target':'transfer','rename':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     configparse = configparser.ConfigParser()
-    configparse.read('./generate.ini',encoding='utf-8')
-    
+    configparse.read('./generate.ini', encoding='utf-8')
+
     url = subscription
     target = input_config['target']
     rename = input_config['rename']
     include = input_config['include']
     exclude = input_config['exclude']
     config = input_config['config']
-    configparse.set(target,'url',url)
-    configparse.set(target,'rename',rename)
-    configparse.set(target,'include',include)
-    configparse.set(target,'exclude',exclude)
-    configparse.set(target,'config',config)
+    configparse.set(target, 'url', url)
+    configparse.set(target, 'rename', rename)
+    configparse.set(target, 'include', include)
+    configparse.set(target, 'exclude', exclude)
+    configparse.set(target, 'config', config)
 
     origin_configparse = configparser.ConfigParser()
-    origin_configparse.read('./generate.ini',encoding='utf-8')
-    origin_config = {'url':origin_configparse[target]['url'],'rename':origin_configparse[target]['rename'],'include':origin_configparse[target]['include'],'exclude':origin_configparse[target]['exclude'],'config':origin_configparse[target]['config']}
+    origin_configparse.read('./generate.ini', encoding='utf-8')
+    origin_config = {'url': origin_configparse[target]['url'], 'rename': origin_configparse[target]['rename'],
+                     'include': origin_configparse[target]['include'], 'exclude': origin_configparse[target]['exclude'],
+                     'config': origin_configparse[target]['config']}
 
     with open('./generate.ini', 'w', encoding='utf-8') as ini:
-        configparse.write(ini,space_around_delimiters=False)
+        configparse.write(ini, space_around_delimiters=False)
 
     if os.name == 'posix':
         args = ['./subconverter-linux-amd64', '-g', '--artifact', target]
     elif os.name == 'nt':
         args = ['.\subconverter-windows-amd64.exe', '-g', '--artifact', target]
-    subconverter = subprocess.Popen(args,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,universal_newlines=True,encoding='utf-8',bufsize=1)
+    subconverter = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True,
+                                    encoding='utf-8', bufsize=1)
     logs = subconverter.stdout.readlines()
     subconverter.wait()
     # Print log
@@ -133,7 +137,7 @@ def subconverterhandler(subscription,input_config={'target':'transfer','rename':
             output = ''
     else:
         try:
-            with open(f'./temp', 'r', encoding= 'utf-8', errors='ignore') as temp_file:
+            with open(f'./temp', 'r', encoding='utf-8', errors='ignore') as temp_file:
                 output = ''
                 while True:
                     content = temp_file.read(100)
@@ -146,22 +150,25 @@ def subconverterhandler(subscription,input_config={'target':'transfer','rename':
         except Exception:
             output = ''
 
-    origin_configparse.set(target,'url',origin_config['url'])
-    origin_configparse.set(target,'rename',origin_config['rename'])
-    origin_configparse.set(target,'include',origin_config['include'])
-    origin_configparse.set(target,'exclude',origin_config['exclude'])
-    origin_configparse.set(target,'config',origin_config['config'])
+    origin_configparse.set(target, 'url', origin_config['url'])
+    origin_configparse.set(target, 'rename', origin_config['rename'])
+    origin_configparse.set(target, 'include', origin_config['include'])
+    origin_configparse.set(target, 'exclude', origin_config['exclude'])
+    origin_configparse.set(target, 'config', origin_config['config'])
     with open('./generate.ini', 'w', encoding='utf-8') as ini:
-        origin_configparse.write(ini,space_around_delimiters=False)
+        origin_configparse.write(ini, space_around_delimiters=False)
 
     os.chdir(work_dir)
     return output
-def deduplicate(clash_provider,keep_nodes=1): # Proxies deduplicate. If proxies with the same servers are greater than keep_nodes, they will not be added.
+
+
+def deduplicate(clash_provider,
+                keep_nodes=1):  # Proxies deduplicate. If proxies with the same servers are greater than keep_nodes, they will not be added.
     lines = re.split(r'\n+', clash_provider)[1:]
     print('Starting deduplicate...')
     print(f'Init amount: {len(lines)}')
     try:
-        proxies = yaml.safe_load(clash_provider)['proxies'] # load all proxies from clash provider
+        proxies = yaml.safe_load(clash_provider)['proxies']  # load all proxies from clash provider
     except Exception:
         il_chars = ['|', '?', '[', ']', '@', '!', '%', ':']
 
@@ -187,12 +194,12 @@ def deduplicate(clash_provider,keep_nodes=1): # Proxies deduplicate. If proxies 
                             value_list_fix.append(value)
                         elif value_il == True and '}' in value:
                             if '}}}' in value:
-                                host_part = value.replace('}}}','')
-                                host_value = '"'+host_part+'"}}}'
+                                host_part = value.replace('}}}', '')
+                                host_value = '"' + host_part + '"}}}'
                                 value_list_fix.append(host_value)
                             elif '}}' not in value:
-                                host_part = value.replace('}','')
-                                host_value = '"'+host_part+'"}'
+                                host_part = value.replace('}', '')
+                                host_value = '"' + host_part + '"}'
                                 value_list_fix.append(host_value)
                         else:
                             value_list_fix.append(value)
@@ -208,18 +215,18 @@ def deduplicate(clash_provider,keep_nodes=1): # Proxies deduplicate. If proxies 
                 except Exception:
                     pass
         fix_provider = '\n'.join(line_fixed)
-        
+
         try:
             proxies = yaml.safe_load(fix_provider)['proxies']
         except Exception:
             print('Deduplicate failed, skip')
             output = clash_provider
             return output
-    
+
     servers = {}
     for proxy in proxies:
-        server = proxy['server'] # assign remote server
-        if server.replace('.','').isdigit():
+        server = proxy['server']  # assign remote server
+        if server.replace('.', '').isdigit():
             ip = server
         else:
             try:
@@ -228,9 +235,9 @@ def deduplicate(clash_provider,keep_nodes=1): # Proxies deduplicate. If proxies 
                 ip = server
 
         if ip in servers:
-            servers[ip].append(proxy) # add proxy to its remote server list
+            servers[ip].append(proxy)  # add proxy to its remote server list
         elif server not in servers:
-            servers[ip] = [proxy] # init remote server list, add first proxy
+            servers[ip] = [proxy]  # init remote server list, add first proxy
 
     proxies = []
     for server in servers:
@@ -248,65 +255,75 @@ def deduplicate(clash_provider,keep_nodes=1): # Proxies deduplicate. If proxies 
             add_list = servers[server]
         for x in add_list:
             proxies.append(x)
-    print(f'Dedupicate success, remove {len(lines)-len(proxies)} duplicate proxies')
+    print(f'Dedupicate success, remove {len(lines) - len(proxies)} duplicate proxies')
     print(f'Output amount: {len(proxies)}')
-    #print(proxies)
-    mapping = {'港':'香港', 'HK':'香港', 'Hong Kong':'香港', 
-			 '坡':'新加坡','SG':'新加坡',
-			 'JP':'日本','日':'日本',
-			 '台':'台湾','TW':'台湾',
-			 '韩':'韩国','KR':'韩国',
-			 'US':'美国','美':'美国',
-			 'CA':'加拿大','加':'加拿大',
-			 '欧':'欧洲','印':'印度','IN':'印度',
-			 'GB':'英国','英':'英国',
-			 'FR':'法国','法':'法国',
-			 'DE':'德国','德':'德国',
-			 'CN':'中国','中国':'中国',
-			 'AU':'澳大利亚','澳':'澳大利亚',
-			 'RU':'俄罗斯','俄':'俄罗斯','RELAY':'其他','NOWHERE':'未知' }
+    # print(proxies)
+    mapping = {'港': '香港', 'HK': '香港', 'Hong Kong': '香港',
+               '坡': '新加坡', 'SG': '新加坡',
+               'JP': '日本', '日': '日本',
+               '台': '台湾', 'TW': '台湾',
+               '韩': '韩国', 'KR': '韩国',
+               'US': '美国', '美': '美国',
+               'CA': '加拿大', '加': '加拿大',
+               '欧': '欧洲', '印': '印度', 'IN': '印度',
+               'GB': '英国', '英': '英国',
+               'FR': '法国', '法': '法国',
+               'DE': '德国', '德': '德国',
+               'CN': '中国', '中国': '中国',
+               'AU': '澳大利亚', '澳': '澳大利亚',
+               'RU': '俄罗斯', '俄': '俄罗斯', 'RELAY': '其他', 'NOWHERE': '未知'}
     for item in proxies:
-	item_name = item['name']
-	for k, v in mapping.items():
-	    if k in item_name:
-		item_name = v
-		break
-	else:
-	    item_name = '其他'
-	item['name'] = item_name
+        item_name = item['name']
+        for k, v in mapping.items():
+            if k in item_name:
+                item_name = v
+                break
+        else:
+            item_name = '其他'
+        item['name'] = item_name
     print(proxies)
     output = yaml.dump({'proxies': proxies}, default_flow_style=False, sort_keys=False, allow_unicode=True, indent=2)
     return output
+
 
 def base64_decode(content):
     if '-' in content:
         content = content.replace('-', '+')
     if '_' in content:
         content = content.replace('_', '/')
-    #print(len(url_content))
+    # print(len(url_content))
     missing_padding = len(content) % 4
     if missing_padding != 0:
-        content += '='*(4 - missing_padding) # 不是4的倍数后加= https://www.cnblogs.com/wswang/p/7717997.html
+        content += '=' * (4 - missing_padding)  # 不是4的倍数后加= https://www.cnblogs.com/wswang/p/7717997.html
     try:
-        base64_content = base64.b64decode(content.encode('utf-8')).decode('utf-8','ignore') # https://www.codenong.com/42339876/
+        base64_content = base64.b64decode(content.encode('utf-8')).decode('utf-8',
+                                                                          'ignore')  # https://www.codenong.com/42339876/
         base64_content_format = base64_content
         return base64_content_format
     except UnicodeDecodeError:
         base64_content = base64.b64decode(content)
         base64_content_format = base64_content
         return str(base64_content)
+
+
 def base64_encode(content):
     if content == None:
         content = ''
     base64_content = base64.b64encode(content.encode('utf-8')).decode('ascii')
     return base64_content
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert between various proxy subscription formats using Subconverter.')
+    parser = argparse.ArgumentParser(
+        description='Convert between various proxy subscription formats using Subconverter.')
     parser.add_argument('--subscription', '-s', help='Your subscription url or local file path.', required=True)
-    parser.add_argument('--target', '-t', help='Target convert format, support base64, clash, clash_provider, quanx.', default='clash')
-    parser.add_argument('--output', '-o', help='Target path to output, default value is the Subconverter root directionary.', default='./Eternity.yaml')
-    parser.add_argument('--deduplicate', '-d', help='Whether to deduplicate proxies, default value is False.', default=False)
+    parser.add_argument('--target', '-t', help='Target convert format, support base64, clash, clash_provider, quanx.',
+                        default='clash')
+    parser.add_argument('--output', '-o',
+                        help='Target path to output, default value is the Subconverter root directionary.',
+                        default='./Eternity.yaml')
+    parser.add_argument('--deduplicate', '-d', help='Whether to deduplicate proxies, default value is False.',
+                        default=False)
     parser.add_argument('--keep', '-k', help='Amounts of nodes to keep when deduplicated.', default=1)
     args = parser.parse_args()
 
@@ -322,11 +339,13 @@ if __name__ == '__main__':
     work_dir = os.getcwd()
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     generate = configparser.ConfigParser()
-    generate.read('./generate.ini',encoding='utf-8')
-    config={'deduplicate': deduplicate_enabled,'keep_nodes': keep_nodes,'rename': generate.get(target,'rename'), 'include': generate.get(target,'include'), 'exclude': generate.get(target,'exclude'), 'config': generate.get(target,'config')}
+    generate.read('./generate.ini', encoding='utf-8')
+    config = {'deduplicate': deduplicate_enabled, 'keep_nodes': keep_nodes, 'rename': generate.get(target, 'rename'),
+              'include': generate.get(target, 'include'), 'exclude': generate.get(target, 'exclude'),
+              'config': generate.get(target, 'config')}
 
-    output = convert(subscription,target,config)
+    output = convert(subscription, target, config)
 
-    with open(output_dir, 'w', encoding= 'utf-8') as temp_file:
+    with open(output_dir, 'w', encoding='utf-8') as temp_file:
         temp_file.write(output)
     os.chdir(work_dir)
