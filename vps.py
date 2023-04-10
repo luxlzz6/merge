fc9ec80e89d6642d867f45e6f216d30ae5401495
luxl-1379 <726857632@qq.com>
2023-04-10 14:20:56 +0800

    Update vps.py

--- vps.py
+++ vps.py
@@ -1,3 +1,6 @@
+import base64
+
+
 # 打开原始文件
 with open('original.txt', 'r') as f1:
     # 读取第一行数据
@@ -31,3 +34,14 @@ with open('original.txt', 'w') as f1:
 
 # 关闭原始文件
 f1.close()
+
+# 读取新文件内容
+with open('new.txt', 'r') as f2:
+    content = f2.read()
+
+# 将内容转为base64格式
+base64_content = base64.b64encode(content.encode('utf-8')).decode('utf-8')
+
+# 将base64格式的内容写入文件
+with open('new_file_base64.txt', 'w') as f3:
+    f3.write(base64_content)
